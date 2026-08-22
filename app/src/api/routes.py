03e10b63535e551d2c80404fa1eabf1f4451b23f
Sagar <sagargurung1001@gmail.com>
2026-08-22 21:05:30 +0100

    chores: app verison for test added in code

--- app/src/api/routes.py
+++ app/src/api/routes.py
@@ -2,6 +2,8 @@ import os
 from flask import Blueprint, jsonify
 
 api_bp = Blueprint("api", __name__)
+#this is just for change in the code and to push to check deployments
+app_version = "v1.0.0"
 
 @api_bp.route("/")
 def index():
@@ -15,3 +17,11 @@ def index():
 def health():
     """Liveness & Readiness probe for K3s / Kubernetes pods."""
     return jsonify({"status": "healthy"}), 200
+
+@api_bp.route('/version', methods=['GET'])
+def get_version():
+    return jsonify(
+        {
+            "app_version" : app_version
+        }
+    ), 200
