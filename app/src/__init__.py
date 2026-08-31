a45749f4cf1460b360c15eb3d351477cdd901b9f
Sagar <sagargurung1001@gmail.com>
2026-08-31 14:05:17 +0100

    feature: added prometheus service monitor for pod

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -1,12 +1,17 @@
 import os
 from flask import Flask
 from src.config import config_by_name
+from prometheus_flask_exporter import PrometheusMetrics
 
 def create_app(config_name=None):
     if config_name is None:
         config_name = os.getenv("ENVIRONMENT", "dev")
 
     app = Flask(__name__)
+
+    # Initialize Prometheus exporter with the Flask app instance
+    metrics = PrometheusMetrics(app)
+    metrics.info('app_info', 'Application info', version='1.0.0')
     
     # Load configuration class
     app.config.from_object(config_by_name.get(config_name, config_by_name["default"]))
